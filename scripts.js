4e6c1bbae2c901aa6696cb59ac6594329c89b230
victorbalbuena <balbuenabellovictorantonio@gmail.com>
2021-03-21 19:34:43 -0600

    fixed

--- scripts.js
+++ scripts.js
@@ -1,22 +1,32 @@
 leerArchivo = (e) => {
+  // files para acceder a la lista de archivos
   let archivo = e.target.files[0];
-if (!archivo) {
-  return;
-}
-let lector = new FileReader();
-lector.onload = function(e) {
-  let contenido = e.target.result;
-  mostrarContenido(contenido);
-};
-lector.readAsText(archivo);
+  // En caso de que no exista un archivo, no hacer nada
+  if (!archivo) {
+    return;
+  }
+
+  // FileReader para leer el contenido de un fie
+  let lector = new FileReader();
+
+  lector.onload = function(e) {
+    let contenido = e.target.result;
+    // ejecuta la función encargada de cargar el contenido en pantalla
+    mostrarContenido(contenido);
+  };
+
+  // Regresa el contenido del archivo
+  lector.readAsText(archivo);
 } 
 
 mostrarContenido = (contenido) => {
-  let elemento = document.getElementById('contenido-archivo');
+  
+  let elemento = document.querySelector('#contenido-archivo');
+  // Mostrar el contenido en el DOM
   elemento.innerHTML = contenido;
+
 }
 
-document.getElementById('file-input')
-  .addEventListener('change', leerArchivo, false);
-
-
+let input = document.querySelector('#file-input');
+// Cada que ocurra un cambio se vuelve a ejecutar la función
+input.addEventListener('change', leerArchivo);
